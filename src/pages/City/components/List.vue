f90ad63c235c19d643bea1c25cc3a227c20095bb
JasonWu008 <1179858091@qq.com>
2020-02-12 13:50:51 +0800

    完成Alphabet组件的逻辑

--- src/pages/City/components/List.vue
+++ src/pages/City/components/List.vue
@@ -38,6 +38,7 @@
           class="area"
           v-for="(item,key) of cities"
           :key="key"
+          :ref="key"
         >
           <div class="title border-topbottom">{{key}}</div>
           <div class="item-list">
@@ -61,10 +62,19 @@ export default {
   name: 'CityList',
   props: {
     cities: Object,
-    hotCities: Array
+    hotCities: Array,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
